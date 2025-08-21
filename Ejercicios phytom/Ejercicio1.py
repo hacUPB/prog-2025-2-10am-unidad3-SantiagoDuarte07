6260f3799e64b30a22b377440f64245f12c1c2b6
Santiago Duarte <santiago.duarted@upb.edu.co>
2025-08-21 11:31:41 -0500

    Ejercicio 2 clasificacion

--- Ejercicios phytom/Ejercicio1.py
+++ Ejercicios phytom/Ejercicio1.py
@@ -15,3 +15,20 @@ altura = float(altura)
 imc = peso/altura**2
 #mostrar imc
 print("tu IMC = ", imc)
+#clasificacion segun el IMC
+if 18.5<=imc<=24.9:
+    print("clasificacion normal")
+else:
+    if 25<=imc<=29.9:
+        print("clasificacion sobre peso")
+    else:
+        if 30<=imc<=34.9:
+            print("clasificacion obesidad 1")
+        else: 
+            if 35<=imc<=39.9:
+                print("Clasificacion obesidad 2")
+            else:
+                if imc>=40:
+                    print("clasificacion obesidad extrema")
+                   
+
