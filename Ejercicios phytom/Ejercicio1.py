7d699946c4b90fabe54919961407ed81227066df
Santiago Duarte <santiago.duarted@upb.edu.co>
2025-08-26 11:28:08 -0500

    trabajo 26/08/2025

--- Ejercicios phytom/Ejercicio1.py
+++ Ejercicios phytom/Ejercicio1.py
@@ -16,19 +16,17 @@ imc = peso/altura**2
 #mostrar imc
 print("tu IMC = ", imc)
 #clasificacion segun el IMC
-if 18.5<=imc<=24.9:
-    print("clasificacion normal")
+if imc<18.5:
+    mensaje = "bajo peso"
+elif imc<25:
+    mensaje = "peso normal"
+elif imc<30:
+    mensaje = "sobre peso"
+elif imc<35:
+    mensaje = "obesidad tipo 1"
+elif imc<40:
+    mensaje = "obesidad tipo 2"
 else:
-    if 25<=imc<=29.9:
-        print("clasificacion sobre peso")
-    else:
-        if 30<=imc<=34.9:
-            print("clasificacion obesidad 1")
-        else: 
-            if 35<=imc<=39.9:
-                print("Clasificacion obesidad 2")
-            else:
-                if imc>=40:
-                    print("clasificacion obesidad extrema")
-                   
+    mensaje = "obesidad extrema"
 
+print(f"Paciente {nombre}, tiene un IMC de {imc:0.2f} y su condicion de {mensaje}")
